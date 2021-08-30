fdd49a384d95000542ef55c18a936634ace3f6b2
genie <luvbliny@gmail.com>
2021-08-30 13:06:13 +0900

    feat : useTab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
 import { useInput } from "./useInput";
+import { useTab } from "./useTab";
+import { content } from "./constant";
 
 function App() {
+  const { currentItem, changeItem } = useTab(0, content);
   const maxLen = (value) => value.length <= 10;
   const isNotEmail = (value) => !value.includes("@");
   const name = useInput("Mr.", isNotEmail);
@@ -10,6 +13,10 @@ function App() {
     <div className="App">
       <h1>Hello</h1>
       <input placeholder="Name" {...name} />
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 }
