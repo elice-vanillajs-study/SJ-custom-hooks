d76ac63d1ff81a7748cded6a941bdcb7f15eaad8
genie <luvbliny@gmail.com>
2021-08-31 18:30:30 +0900

    feat : useFullscreen

---
+++ src/hooks/useFullscreen.js
@@ -0,0 +1,38 @@
+import React, { useRef } from "react";
+
+export const useFullscreen = (callback) => {
+  const element = useRef();
+  const runCb = (isFull) => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
+  };
+  const triggerFull = () => {
+    if (element.current) {
+      if (element.current.requestFullscreen) {
+        element.current.requestFullscreen();
+      } else if (element.current.mozRequestFullScreen) {
+        element.current.mozRequestFullScreen();
+      } else if (element.current.webkitRequestFullScreen) {
+        element.current.webkitRequestFullScreen();
+      } else if (element.current.msRequestFullScreen) {
+        element.current.msRequestFullScreen();
+      }
+	  runCb(true);
+    }
+  };
+  const exitFull = () => {
+    if (document) {
+		if (document.exitFullscreen) {
+		  document.exitFullscreen();
+		} else if (document.mozCancelFullScreen) {
+		  document.mozCancelFullScreen();
+		} else if (document.webkitExitFullScreen) {
+		  document.webkitExitFullScreen();
+		} else if (document.msExitFullScreen) {
+		  document.msExitFullScreen();
+		}
+		runCb(false);
+  };
+  return { element, triggerFull, exitFull };
+};
